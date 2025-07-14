62fdd8cefe7f50651fda21c262bdcfe30d7c61f5
bdkleo <mizuo6188@gmail.com>
2025-07-14 17:15:35 +0800

    顺序表练习

---
+++ C_data_r1/C_data_r1/SeqList.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+
+typedef int SLDataType;
+
+typedef struct SeqList
+{
+	SLDataType* arr;
+	int size;
+	int capacity;
+}SL;
+
+//初始化
+void SLInit(SL* ps);
+
+//申请空间
+void SLCheakCapacity(SL* ps);
+
+//尾插
+void SLPushBack(SL* ps, SLDataType x);
+
+//头插
+void SLPushFront(SL* ps,SLDataType x);
+
+//尾删
+void SLPopBack(SL* ps);
+
+//头删
+void SLPopFront(SL* ps);
+
+//打印
+void SLPrint(SL s);
+
+//销毁
+void SLDestroy(SL* ps);
---
+++ C_data_r1/C_data_r1/test.c
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include"SeqList.h"
+
+void Test01()
+{
+	//初始化
+	SL sl;
+	SLInit(&sl);
+
+	//尾插
+	SLPushBack(&sl, 1);
+	SLPushBack(&sl, 2);
+	SLPushBack(&sl, 3);
+	SLPushBack(&sl, 4);
+	SLPushBack(&sl, 5);
+
+	//头插
+	SLPushFront(&sl,0);
+	SLPushFront(&sl,-1);
+	SLPrint(sl);
+
+	//尾删
+	SLPopBack(&sl);
+
+	//头删
+	SLPopFront(&sl);
+	SLPrint(sl);
+
+	//销毁
+	SLDestroy(&sl);
+}
+
+int main()
+{
+	Test01();
+	return 0;
+}
